68c8bd0c23ec8d8d2e52ebe996237f4d55fea51e
vimu-nk <vnkaushalya0705@gmail.com>
2024-08-17 12:25:56 +0530

    Modified player structure and added struct to pieces

--- game.c
+++ game.c
@@ -8,10 +8,10 @@ int main()
 {
     struct players player[4] =  //0 - Yellow, 1 - Blue, 2 - Red, 3 - Green
     {
-        {"Yellow", 'Y', 0, -1, 0, -1, 0, -1, 0, -1, 0},
-        {"Blue", 'B', 0, -1, 0, -1, 0, -1, 0, -1, 0},
-        {"Red", 'R', 0, -1, 0, -1, 0, -1, 0, -1, 0},
-        {"Green", 'G', 0, -1, 0, -1, 0, -1, 0, -1, 0}
+        {"Yellow", 'Y', 0, {1, -1, 0, 2, -1, 0, 3, -1, 0, 4, -1, 0}, 4, 0},
+        {"Blue", 'B', 0, {1, -1, 0, 2, -1, 0, 3, -1, 0, 4, -1, 0}, 4, 0},
+        {"Red", 'R', 0, {1, -1, 0, 2, -1, 0, 3, -1, 0, 4, -1, 0}, 4, 0},
+        {"Green", 'G', 0, {1, -1, 0, 2, -1, 0, 3, -1, 0, 4, -1, 0}, 4, 0}
     };
 
     short playOrder[4];
@@ -33,7 +33,9 @@ int main()
     }
 
     printf("%s has the highest roll and will begin the game.\n", player[playOrder[0]].colour);
-    printf("The order of a single round is %s, %s, %s, and %s.\n", player[playOrder[0]].colour, player[playOrder[1]].colour, player[playOrder[2]].colour, player[playOrder[3]].colour);
+    printf("The order of a single round is %s, %s, %s, and %s.\n\n", player[playOrder[0]].colour, player[playOrder[1]].colour, player[playOrder[2]].colour, player[playOrder[3]].colour);
+
+    printf("Colour : %s\nLetter : %c\nDice Value : %d\nPiece %d : (Pos : %d, Dir : %d)\nPiece %d : (Pos : %d, Dir : %d)\nPiece %d : (Pos : %d, Dir : %d)\nPiece %d : (Pos : %d, Dir : %d)\n", player[0].colour, player[0].letter, player[0].diceVal, player[0].pieces[0].index, player[0].pieces[0].position, player[0].pieces[0].pieceDir, player[0].pieces[1].index, player[0].pieces[1].position, player[0].pieces[1].pieceDir, player[0].pieces[2].index, player[0].pieces[2].position, player[0].pieces[2].pieceDir, player[0].pieces[3].index, player[0].pieces[3].position, player[0].pieces[3].pieceDir);
 
     return 0;
 }
--- types.h
+++ types.h
@@ -1,19 +1,21 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+struct playerPieces
+{
+    short index;
+    short position;
+    short pieceDir;
+};
+
 struct players
 {
     char colour[7];
     char letter;
     short diceVal;
-    short piece1;
-    short piece1Dir;
-    short piece2;
-    short piece2Dir;
-    short piece3;
-    short piece3Dir;
-    short piece4;
-    short piece4Dir;
+    struct playerPieces pieces[4];
+    short piecesBase;
+    short piecesFinished;
 };
 
 short diceRoll();
